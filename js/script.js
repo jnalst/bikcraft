44f5ab97ef04ec92bbc4bf08bf85d6f7df8bf2c1
Juan Almeida <117415730+jnalst@users.noreply.github.com>
2023-10-18 16:47:26 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -50,7 +50,23 @@ perguntas.forEach(eventosPerguntas);
 const galeria = document.querySelectorAll(".bicicleta-imagens img");
 const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
-console.log(galeria, galeriaContainer);
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+  galeriaContainer.prepend(img);
+  }
+}
 
+function eventosGaleria(img) {
+  img.addEventListener('click', trocarImagem);
+}
 
+galeria.forEach(eventosGaleria);
+
+// Animação
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
 
